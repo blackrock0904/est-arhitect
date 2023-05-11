e8993ff6792c78620c4fbf7baf8dd6316adcc4dd
Maks Shevchenko <shevchenko@alarstudios.com>
2023-05-11 11:00:40 +0200

    about page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
-const isFirstLoad = !sessionStorage.getItem("firsLoad");
+const isSecondLoad = sessionStorage.getItem("secondLoad");
+if (isSecondLoad) {
+    const preloader = document.getElementById("preloader");
+    preloader && preloader.classList.add("hidden");
+}
 window.scrollTo(0, 0);
 
 window.addEventListener("DOMContentLoaded", function () {
+    const menu = document.getElementById("menu")
+    if (menu) {
+        menu.addEventListener("click", (e) => {
+            if (e.target.className.includes("active")) {
+                e.preventDefault()
+            };
+        });
+    }
+
     initAnimation();
     const preloader = document.getElementById("preloader");
     preloader && preloader.classList.add("loaded");
@@ -32,14 +45,23 @@ function initAnimation() {
     const container = document.getElementById("section-hero");
     container && (container.style.height = '100vh');
     window.addEventListener("resize", () => container && (container.style.height = '100vh'));
+    if (container && !isSecondLoad) {
+    document.querySelectorAll('.hero-animate')
+        .forEach(el => setTimeout(() => {
+            const spinning = [
+                { transform: "translateX(-100px)" },
+                { transform: "translateX(0)" },
+            ];
+            const timing = {
+                duration: 3000,
+                iterations: 1,
+            };
+            el.animate(spinning, timing)
+        },3000));
+    }
 
     const bgSlider = document.querySelector(".section-hero__bg-slider");
     bgSlider && (bgSlider.style.backgroundImage = `url(${bgSlider.dataset.src})`);
 
-    // if (container && isFirstLoad) {
-        document.querySelectorAll('.hero-animate')
-            .forEach(el => setTimeout(() => el.classList.add('animated'),3000));
-        sessionStorage.setItem("firsLoad", "true");
-    // }
-
+    sessionStorage.setItem("secondLoad", "true");
 }
